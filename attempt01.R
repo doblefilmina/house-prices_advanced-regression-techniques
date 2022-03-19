a3aea242be4eb836dedf0f7fc2892be0c1e3d4fb
doblefilmina <brusafederico@gmail.com>
2022-03-19 10:43:22 -0300

    starting kaggle competition

---
+++ attempt01.R
@@ -0,0 +1,10 @@
+##clear
+rm(list=ls())
+
+##imports
+
+
+##set directory
+setwd("C:\Users\New\Documents\kaggle\house-prices_advanced-regression-techniques")
+
+##load data
---
+++ attempt01.py
@@ -0,0 +1,6 @@
+##Libraries
+import os
+
+##set directory
+print(os.getcwd())
+os.chdir("c:/Users/New/Documents/kaggle/house-prices_advanced-regression-techniques")
