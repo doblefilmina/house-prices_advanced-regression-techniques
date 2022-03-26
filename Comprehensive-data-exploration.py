3bc583b7b73c0558a348ce124f60e6c792498ec8
doblefilmina <brusafederico@gmail.com>
2022-03-26 09:06:27 -0300

    data exploration

--- Comprehensive-data-exploration.py
+++ Comprehensive-data-exploration.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import os
 from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
 from scipy import stats
 import warnings
-warnings.filterwarnings('ignore')
+#warnings.filterwarnings('ignore')
 
 #%matplotlib inline
 
@@ -22,6 +23,69 @@ df_train.columns
 
 ## analyse SalePrice
 df_train['SalePrice'].describe()
+
+##histogram
+sns.distplot(df_train['SalePrice'])
+
+##boxplot
+sns.boxplot(df_train['SalePrice'])
+
+##skewness and kurtosis
+print('Skewness: %f' % df_train['SalePrice'].skew())
+print('Kurtosis: %f' % df_train['SalePrice'].kurt())
+
+####Relationship with numerical variables
+##Scatter plot grlivarea/saleprice
+var = 'GrLivArea'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0 , 800000))
+
+##Scatter plot totalbsmtsf/saleprice
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+
+####Relationship with categorical features
+#box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8,6))
+fig = sns.boxplot(x=var, y='SalePrice', data=data)
+fig.axis(ymin=0, ymax=800000)
+
+#box plot YearBuilt/SalePrice
+var = 'YearBuilt'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+f, ax = plt.subplots(figsize=(20,8))
+fig = sns.boxplot(x=var, y='SalePrice', data = data)
+fig.axis(ymin=0, ymax=800000);
+plt.xticks(rotation=90);
+
+####Correlation matrix (heatmap style)
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12,9))
+sns.heatmap(corrmat, vmax=0.8, square=True)
+
+#Saleprice correlation matrix (zoomed heatmap style)
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='2f', annot_kws ={'size':10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
+
+#Scatterplot
+sns.set()
+cols=['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show()
+
+#### Missing data
+total = df_train.isnull().sum().sort_values(ascending=False)
+percent = (df_train.isnull().sum() / df_train.isnull().count() ).sort_values(ascending=False)
+missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+missing_data.head(20)
+
 
 
 
